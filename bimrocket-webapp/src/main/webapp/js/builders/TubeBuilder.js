41e9ded1f25e0b342262e0bc3266bc04528034c0
jiponsI2cat <josep.pons@i2cat.net>
2026-02-26 17:49:25 +0000

    optimising z-fighting

--- bimrocket-webapp/src/main/webapp/js/builders/TubeBuilder.js
+++ bimrocket-webapp/src/main/webapp/js/builders/TubeBuilder.js
@@ -7,64 +7,74 @@ import { ObjectBuilder } from "./ObjectBuilder.js";
 import * as THREE from "three";
 
 class TubeBuilder extends ObjectBuilder {
-  constructor(radius = 6.0, zscale = 0.001, zoffset = -1) {
+  constructor(radius = 6.0, zScale = 0.001, zOffset = -1, segmentsMultiplier = 4, profileEdges = 8, looping = false) {
     super();
     this.type = "tube";
     this.radius = radius;
-    this.zscale = zscale;
-    this.zoffset = zoffset;
+    this.zScale = zScale;
+    this.zOffset = zOffset;
+    this.segmentsMultiplier = segmentsMultiplier;
+    this.profileEdges = profileEdges;
+    this.looping = looping;
   }
 
   performBuild(object) {
     let source = object;
-    if (!object.geometry || !object.geometry.type.includes("CordGeometry")) {
+    if (!object.geometry || !object.geometry.type.includes("CordGeometry")) 
+    {
       source = object.children.find(c => c.geometry && c.geometry.type.includes("CordGeometry"));
     }
 
-    if (!source || !source.geometry) return;
+    if (source && source.geometry && source.geometry.type.includes("CordGeometry"))
+    {
+      try {
+        const attr = source.geometry.attributes.position;
+        if (!attr) return;
 
-    try {
-      const attr = source.geometry.attributes.position;
-      if (!attr) return;
-
-      const points = [];
-      for (let i = 0; i < attr.count; i++) {
-        const v = new THREE.Vector3().fromBufferAttribute(attr, i);
-        // Avoid duplicated points that break the curve
-        if (v && (points.length === 0 || v.distanceTo(points[points.length - 1]) > 0.01)) {
-          points.push(v);
-        }
-      }
-
-      if (points.length > 1) {
-        const curve = new THREE.CatmullRomCurve3(points);
-        // Creamos la geometría del tubo
-        const tubeGeo = new THREE.TubeGeometry(curve, points.length * 4, this.radius, 8, false);
-        
-        // Asignamos la nueva geometría al objeto principal
-        object.geometry = tubeGeo;
-
-        if (object.material) {
-          object.material.wireframe = false;
-          object.material.needsUpdate = true;
+        const points = [];
+        for (let i = 0; i < attr.count; i++) {
+          const v = new THREE.Vector3().fromBufferAttribute(attr, i);
+          // Avoid duplicated points that break the curve
+          if (v && (points.length === 0 || v.distanceTo(points[points.length - 1]) > 0.01)) {
+            points.push(v);
+          }
         }
 
-        object.edgesVisible = false;
+        if (points.length > 1) {
+          const curve = new THREE.CatmullRomCurve3(points);
+          const tubeGeo = new THREE.TubeGeometry(curve, points.length * this.segmentsMultiplier, this.radius, this.profileEdges, this.looping);
+          object.geometry = tubeGeo;
+          
+          if (object.material) {
+            object.material.wireframe = false;
+            object.material.needsUpdate = true;
+          }
+          
+          object.edgesVisible = false;
 
-        //plain tube 
-        object.scale.set(1, 1, this.zscale);
-        //avoid Z-fighting
-        object.position.z -= this.zoffset;
+          //plain tube 
+          object.scale.set(1, 1, this.zScale);
+          //avoid Z-fighting
+          let randomOffset = this.zOffset + Math.floor(Math.random() * 10);
+          object.position.z += randomOffset;
 
-        // if source is child (WFS ADD_OBJECT mode), we hidden it to avoid duplication
-        if (source !== object) source.visible = false;
-        
-        object.updateMatrix();
+
+          // if source is child (WFS ADD_OBJECT mode), we hidden it to avoid duplication
+          if (source !== object) source.visible = false;
+          
+          object.updateMatrix();
+          object.updateMatrixWorld(true);
+        }
+      } catch (e) {
+        console.error("TubeBuilder Error: Tube generation from Cord failed. Details: ", e);
       }
-    } catch (e) {
-      console.error("TubeBuilder Error:", e);
+    }
+    else {
+      console.error("TubeBuilder Error: Object neither children do not contain CordGeometry");
     }
   }
+
+    
 
   clone() {
     return new TubeBuilder(this.radius);
