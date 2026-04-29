3fa411a2ed89f682692030bb7d394e57e06fe9c8
miguel-diaz-i2cat <miguel.diaz@i2cat.net>
2026-04-29 15:00:42 +0200

    Add comments to Environmets.js files

--- bimrocket-webapp/docker/Environment.js
+++ bimrocket-webapp/docker/Environment.js
@@ -6,3 +6,13 @@ export const Environment =
   SERVER_ALIAS : "bimrocket",
   MODULES : ["base", "bim", "gis"]
 };
+
+/**
+ * Optional: 'AUTH_ENVIRONMENT' can be added to configure multiple authentication providers.
+ * * Each key in this object represents the system's host (e.g., "http://bim.santfeliu.cat").
+ * It is crucial that this key matches the actual environment host; otherwise, the 
+ * authorization buttons for these services will not be displayed.
+ * * For each host, you can define different authorization methods (such as VALID, 
+ * GICAR, or Keycloak) by specifying their OAuth2 parameters: authorization URL, 
+ * client ID, scopes, and the redirect URI for the authentication code.
+ */
